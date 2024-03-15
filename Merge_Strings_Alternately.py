ca3f7a50e52b198a1311033a531256938ede3d7d
Saaji <saajid.aliyar@outlook.com>
2024-03-14 21:18:35 -0400

    optimal solution

--- Merge_Strings_Alternately.py
+++ Merge_Strings_Alternately.py
@@ -6,14 +6,19 @@ class Solution(object):
         :rtype: str
         """
         str = ""
-        for index, x in enumerate(word1):
-            if word1[index] is None:
-                str = str+word2
-            if word2[index] is None:
-                str = str+word1
-            str = str + word1[index]+word2[index]
-        return str
+
+        diff = len(word1) - len(word2) #get difference 
+        minimumWord = min(len(word1),len(word2)) #minimum of both words
+
+        for i in range(minimumWord):
+            str = str + word1[i]+word2[i]
+        if diff == 0:
+            return str
+        elif diff > 0:
+            return str+word1[len(word2):]    
+        else:
+                return str+word2[len(word1):]
 
 
 s = Solution()
-print(s.mergeAlternately("abc","pq"))
+print(s.mergeAlternately("ab","pqrs"))
