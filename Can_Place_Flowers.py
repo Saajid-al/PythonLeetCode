7e901d1d38e83c3ebe0d82a99329b264c4a6a761
Saaji <saajid.aliyar@outlook.com>
2024-03-17 05:42:12 -0400

    Starting code

---
+++ Can_Place_Flowers.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        
+s = Solution()
+print(s.canPlaceFlowers([1,0,0,0,1], 1))
