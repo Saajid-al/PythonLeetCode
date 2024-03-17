f0f408c23fa3430a0494f3715890ba0d5ca6e52c
Saaji <saajid.aliyar@outlook.com>
2024-03-17 05:35:05 -0400

    Initial commit

---
+++ Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        maxCandy = max(candies)
+        candyList = list()
+        for i in range(candies):
+            if(candies[i] + extraCandies >= candies):
+                candyList.append(True)
+            else:
+                candyList.append(False)
+        return candyList
+            
+            
+
+
+        
+s = Solution()
+print(s.kidsWithCandies("2,3,5,1,3",3))
