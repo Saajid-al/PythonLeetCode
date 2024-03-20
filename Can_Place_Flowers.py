c1e31e357433496dfd02745f38020795ca4e17c4
Saaji <saajid.aliyar@outlook.com>
2024-03-19 23:30:58 -0400

    Not optimal solution, but works

--- Can_Place_Flowers.py
+++ Can_Place_Flowers.py
@@ -5,6 +5,38 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        print (n)
+        #keep a previous holder
+        previous = 0
+        potentialSpot = 0
+        for idx, i  in enumerate(flowerbed[:len(flowerbed)-1]):
+            print("Previous Num : " , previous)
+            nextElem = flowerbed[idx + 1]
+            if i == 0 and nextElem == 0 and previous == 0:
+                print("SUCCESS")
+                potentialSpot = potentialSpot + 1
+                previous = i
+                if(potentialSpot) == n:
+                    return True
+            previous = i
+            print("Current Index" , idx)
+            print("Next Index", nextElem)
+            print("____NEXT____")
+        return False
+
+                
+
+
+             #[1 0 0 0 1]
+             #[0 1 2 3 4]
+                   
         
+    #what we can do is just check how many spots are available
+    #if index[i+1] and index [i-1] == 0
+    #add that to our counter
+    #then if it is less than or equal to our counter
+    #we can return true or false
+                
+
 s = Solution()
 print(s.canPlaceFlowers([1,0,0,0,1], 1))
