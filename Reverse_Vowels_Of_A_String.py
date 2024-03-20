1d10cb9f7eb09c382a4db2a299363950505e6ae7
Saaji <saajid.aliyar@outlook.com>
2024-03-20 03:02:05 -0400

    Solution, can it be more optimal?

--- Reverse_Vowels_Of_A_String.py
+++ Reverse_Vowels_Of_A_String.py
@@ -4,6 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
+
+        vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+        arr = []
+        sList = list(s)
+        for i in range(len(sList)):
+            if sList[i] in vowels:
+                arr.append(sList[i])
+
+        for i in range(len(sList)):
+            if sList[i] in vowels:
+                sList[i] = arr.pop()
+        s = ''.join(sList)
+        return(s)
+
 s = Solution()
 print(s.reverseVowels("Hello"))
+
+#record index ?
+#reverse the string
+#and put back into the index ? 
