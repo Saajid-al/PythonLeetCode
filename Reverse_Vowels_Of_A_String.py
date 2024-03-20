24217df652a18a01caf245b1b8143868e2d58fcf
Saaji <saajid.aliyar@outlook.com>
2024-03-20 02:37:59 -0400

    Initial code

---
+++ Reverse_Vowels_Of_A_String.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+s = Solution()
+print(s.reverseVowels("Hello"))
