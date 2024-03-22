59f0c6e8930ab220aad4fcc4c1f0a5a75575ae5d
Saaji <saajid.aliyar@outlook.com>
2024-03-22 06:05:10 -0400

    horrible way to do this, need to scale down to O(N)

--- Product_Of_Array_Except_Shelf.py
+++ Product_Of_Array_Except_Shelf.py
@@ -4,8 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+
+        result = 1
+        zeroCounter = []
         for idx, x in enumerate(nums):
-            print(nums[idx])
+            if(x == 0):
+                zeroCounter.append(idx)
+                continue
+            result = result * x
+
+        if(len(zeroCounter) > 1):
+            return [0] * len(nums)
+
+        for idx, y in enumerate(nums):
+            if 0 in nums:
+                if nums[idx] == 0:
+                    nums[idx] = result
+                    continue
+                elif nums[idx]!= 0:
+                    nums[idx] = 0
+                    continue
+        nums[idx] = result//nums[idx]
+        return nums
+            
             
 s = Solution()
-print(s.productExceptSelf([1,2,3,4]))
+print(s.productExceptSelf([-1,1,0,-3,0]))
