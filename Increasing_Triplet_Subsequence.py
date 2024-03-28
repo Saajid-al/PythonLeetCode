9b8443095c169985d7dd98f990cb03f23204e6de
Saaji <saajid.aliyar@outlook.com>
2024-03-28 06:00:45 -0400

    initial commit

---
+++ Increasing_Triplet_Subsequence.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+s = Solution(object)    
+print(s.increasingTriplet([[1,2,3,4,5]]))
