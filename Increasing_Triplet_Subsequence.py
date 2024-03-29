b057d929fc2eb0fb44287387ff872d86f8734917
Saaji <saajid.aliyar@outlook.com>
2024-03-29 05:14:40 -0400

    need to review this one too

--- Increasing_Triplet_Subsequence.py
+++ Increasing_Triplet_Subsequence.py
@@ -4,27 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        min = nums[0]
-        maxi = nums[0]
-
-        for i in range(0,len(nums)-1, 1):
-            for j in range(1, len(nums)-1, 1):
-                if nums[i] < nums[j] < nums[j+1]:
-                    return True
+        #create a min array
+        #create a max array
+        #loop through !
+        
+        minI = 10**20
+        minJ = 10**20
+        #i=20 
+        #20, 100, 10, 12, 5, 13
+        for num in nums:
+            if num <= minI:
+                minI = num
+            elif num <= minJ:
+                minJ = num
+            else:
+                return True
         return False
 
-        # for i in range(0, len(nums)-1, 1):
-        #     if(nums[i] < min):
-        #         min = nums[i]
-        #     if(nums[i]+1 > maxi):
-        #         maxi = nums[i+1]
-            
-        #     if(min < nums[i] < maxi):
-        #         return True
-        # return False
-        
 s = Solution()    
-print(s.increasingTriplet([1,2,3,4,5]))
+print(s.increasingTriplet([20,100,10,12,5,13]))
 
 #-3,4,6,7,8,-3,2
 #for every element
