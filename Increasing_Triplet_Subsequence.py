4c41d3b9512ff292b6267688bb54208d8062eff9
Saaji <saajid.aliyar@outlook.com>
2024-03-29 04:25:33 -0400

    dont do brute force this will take way too long

--- Increasing_Triplet_Subsequence.py
+++ Increasing_Triplet_Subsequence.py
@@ -4,6 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        min = nums[0]
+        maxi = nums[0]
+
+        for i in range(0,len(nums)-1, 1):
+            for j in range(1, len(nums)-1, 1):
+                if nums[i] < nums[j] < nums[j+1]:
+                    return True
+        return False
+
+        # for i in range(0, len(nums)-1, 1):
+        #     if(nums[i] < min):
+        #         min = nums[i]
+        #     if(nums[i]+1 > maxi):
+        #         maxi = nums[i+1]
+            
+        #     if(min < nums[i] < maxi):
+        #         return True
+        # return False
         
-s = Solution(object)    
-print(s.increasingTriplet([[1,2,3,4,5]]))
+s = Solution()    
+print(s.increasingTriplet([1,2,3,4,5]))
+
+#-3,4,6,7,8,-3,2
+#for every element
+#set i to be smallest
