6fb5e94470c01681e1f6933c022d719312afc8e2
Saaji <saajid.aliyar@outlook.com>
2024-04-02 02:15:10 -0400

    works but idk how it's supposed to delay, have to fix formatting maybe?

--- String_Compression.py
+++ String_Compression.py
@@ -4,8 +4,27 @@ class Solution(object):
         :type chars: List[str]
         :rtype: int
         """
-        for c in chars:
-            print(c)
+        list1 = []
+        list2 = []
+        temp = "a"
+        counter = 0
+        for idx, c in enumerate(chars):
+            if(c == temp):
+                counter += 1
+                if (idx+1 == len(chars)):
+                    print("ok ok ")
+                    list1.append(chars[idx])
+                    list2.append(counter)
+                    return list1, list2
+            elif (c!= temp):
+                list1.append(chars[idx-1])
+                list2.append(counter)
+                temp = c
+                counter = 1
+
+
+
+
         
 s = Solution()
 print(s.compress(["a","a","b","b","c","c","c"]))
