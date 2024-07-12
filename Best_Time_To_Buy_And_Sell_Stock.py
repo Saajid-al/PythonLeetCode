59f1a4195947dd87c16adf8ae575905a95deae10
Saaji <saajid.aliyar@outlook.com>
2024-07-11 22:51:42 -0400

    eh

---
+++ Best_Time_To_Buy_And_Sell_Stock.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        print(prices[0])
+        
+s = Solution()
+print(s.maxProfit([7,1,5,3,6,4]))
