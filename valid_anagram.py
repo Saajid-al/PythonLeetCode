4aa1ef287c3c469d8ee0cd71a143270048c61446
Saaji <saajid.aliyar@outlook.com>
2024-07-27 03:05:50 -0400

    init

---
+++ valid_anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        print("hello")
+        
+s = Solution()
+s.isAnagram("anagram","nagaram")
