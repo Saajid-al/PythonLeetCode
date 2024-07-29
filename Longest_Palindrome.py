f0bebff9c869c2976504fa7fd78532ad6e352c56
Saaji <saajid.aliyar@outlook.com>
2024-07-29 02:42:25 -0400

    init

---
+++ Longest_Palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+s = Solution()
+s.longestPalindrome("abccccdd")
