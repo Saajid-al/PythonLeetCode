bf9addb103357575cff8aa8fedcf43574fec4221
Saaji <saajid.aliyar@outlook.com>
2024-09-30 17:17:44 -0400

    valid paranthesis & various

--- max_Water_Container.py
+++ max_Water_Container.py
@@ -4,10 +4,10 @@ class Solution:
         r = len(height) - 1
         max_area = 0
         while l < r:
-            length = r - l
-            maxHeight = min(height[l], height[r])
-            max_area = max(max_area,maxHeight*length)
-            if height[l] > height[r]:
+            length = r - l 
+            maxHeight = min(height[l], height[r]) #get the min value, because thats the only way area can be calculated
+            max_area = max(max_area,maxHeight*length) # get max area using length
+            if height[l] > height[r]: #to keep checking we just compare values and increment accordingly
                 r -= 1 
             else:
                 l += 1 
--- valid_parentheses.py
+++ valid_parentheses.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
+        stack = []
+        hashmap = { ')':'(', '}':'{', ']':'['}
+
+        for c in s:
+            if c in hashmap:
+                popped = stack.pop()
+                if popped != hashmap[c]:
+                    return False
+            else:
+                stack.append(c)
+        if stack.isEmpty():
+            return True
+        else:
+            return False
+
+
+
+
+        #go through the list, if the first index has a clos
 s = Solution()
 print(s.isValid("()[]{}"))
