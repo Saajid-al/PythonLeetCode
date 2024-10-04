283552e9463488e6658b0f5fe06fadcdeca7324b
Saaji <saajid.aliyar@outlook.com>
2024-10-04 15:10:23 -0400

    pushed remaining code

---
+++ Daily_Temperatures.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        ans = []
+        for i, val in enumerate(temperatures):
+            if not stack:
+                stack.append(temperatures[i])
+            if stack and stack.pop() > val { 
+                
+            }
+
+s = Solution()
+print(s.dailyTemperatures[73,74,75,71,69,72,76,73])
+            
+            
