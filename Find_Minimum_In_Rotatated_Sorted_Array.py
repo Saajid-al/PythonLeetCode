bccc9b088732280c1b52191bb7b9606d61f64300
Saaji <saajid.aliyar@outlook.com>
2024-10-13 18:46:22 -0400

    review again

---
+++ Find_Minimum_In_Rotatated_Sorted_Array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        l = 0
+        r = len(nums) -1
+
+        res = nums[0]
+
+        while(l <= r):
+            
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if(nums[m] >= nums[r]):
+                l = m+1
+            else:
+                r = m-1 
+        return res
+
+s = Solution()
+print(s.findMin([3,4,5,1,2]))
