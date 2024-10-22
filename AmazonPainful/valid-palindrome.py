693db0ddd7ebb4562400c039eafd51f2f10f6ca6
Saaji <saajid.aliyar@outlook.com>
2024-10-22 16:51:18 -0400

    str manip, use alnum

---
+++ AmazonPainful/valid-palindrome.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        s = ''.join(char.lower() for char in s if char.isalnum())
+        return s==s[::-1]
+    
+s = Solution()
+print(s.isPalindrome("A man, a plan, a canal: Panama"))
