4b4b611d0729b60c2ac75c0791dcd4403853f6f2
Saaji <saajid.aliyar@outlook.com>
2024-10-22 00:48:31 -0400

    try this again maybe one time

---
+++ AmazonPainful/valid_parantheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype
+        """
+
+        stack = []
+
+        map = { ')':'(', ']':'[','}':'{' }
+
+        for i in s:
+            if i in map:
+                if stack[-1] == map[c]:
+                    stack.pop
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
+
+        #([])
