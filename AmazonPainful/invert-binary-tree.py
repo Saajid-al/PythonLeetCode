6e89bc1a1b4cd7d1c9b793438eb93e67a2e03711
Saaji <saajid.aliyar@outlook.com>
2024-10-23 18:57:47 -0400

    tree question

---
+++ AmazonPainful/invert-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        #call root.left = root.right 
+        #call invert tree on that node
+
+        if not root:
+            return None
+        
+        tmp = root.left
+        root.left = root.right
+        tmp = root.right
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
