1504c6b6ae4566d48bb2f84f5c428ecf69f6fc87
Saaji <saajid.aliyar@outlook.com>
2024-10-23 01:26:50 -0400

    used two pointers / sliding window

---
+++ AmazonPainful/longest-substring-without-repeating-characters.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        
+        l = 0
+        r = 0 
+        maxLen = 0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1 
+            charSet.add(s[r])
+            maxLen = max(maxLen, r-l + 1 )
+        return maxLen
+
+
+s = Solution()
+print(s.lengthOfLongestSubstring("abcabcbb"))
