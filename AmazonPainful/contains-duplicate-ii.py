2c7fcecc0cee827d765a9832aa77c8f380c22f77
Saaji <saajid.aliyar@outlook.com>
2024-10-24 01:58:39 -0400

    used set to see if exists in the set. Also used sliding window to find answer

---
+++ AmazonPainful/contains-duplicate-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        hashset = set()
+        l = 0
+        for r in range(len(nums)):
+            if r-l > k:
+                hashset.remove(nums[l])
+                l+=1
+            if nums[r] in hashset:
+                return True
+            hashset.add(nums[r])
+
+        return False
+    
+s = Solution()
+print(s.containsNearbyDuplicate([1,2,3,1,2,3], 2))
