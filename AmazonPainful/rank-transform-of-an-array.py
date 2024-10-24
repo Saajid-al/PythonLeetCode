f72663f35479eea9453bb078b95f42c1829ff8bd
Saaji <saajid.aliyar@outlook.com>
2024-10-24 00:03:25 -0400

    use enumerate to change in place

--- AmazonPainful/rank-transform-of-an-array.py
+++ AmazonPainful/rank-transform-of-an-array.py
@@ -12,10 +12,27 @@ class Solution(object):
             numbersToRank[num] = rank
             rank += 1
         
-
-        for i in range(len(arr)):
-            arr[i] = numbersToRank[arr[i]]
         
+        for idx, val in enumerate(arr):
+            arr[idx] = numbersToRank[val]
         return arr
+
+
+        # for b in arr:
+        #     newList.append(hash[b])
+
+        # return newList
+        
+
+        #go through each element
+        # set the index equal the value
+        # the key is the current element
+        # for each element in array, we can return our result 
+
+
+        #remove duplicates
+
+
+
 s = Solution()
 print(s.arrayRankTransform([40,10,20,30]))
