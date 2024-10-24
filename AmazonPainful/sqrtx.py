27749a74669a232353115f7cace762df77948e31
Saaji <saajid.aliyar@outlook.com>
2024-10-24 01:27:59 -0400

    do again- bin search

--- AmazonPainful/sqrtx.py
+++ AmazonPainful/sqrtx.py
@@ -8,13 +8,13 @@ class Solution(object):
         res = 0
         while l<= r:
             m = l + ((r-l) //2)
-            if m**2 > x:
-                r = m-1
-            elif m**2 < x:
-                l = m+1
-                res = m
+            if m**2 > x: #check if the square of the number is greater than x
+                r = m-1 #adjust binary search
+            elif m**2 < x: #if it's not the biggest, it MIGHT be the number ,so we grab the largest number before it goes over x
+                l = m+1 
+                res = m #set rsult to m
             else:
-                return m  
+                return m  #might actually be our result so we return mid
         return res
     
 s = Solution()
